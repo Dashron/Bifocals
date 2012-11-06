e91ff54b95052250d4f8c50ca56d51c87f0946ba
Aaron Hedges <aaron@dashron.com>
2012-11-05 23:46:42 -0500

    move express stuff into it's own file

--- bifocals.js
+++ bifocals.js
@@ -590,42 +590,12 @@ Renderer.prototype.end = function renderer_end(fn) {
  * }));
  *
  * 
- * 
  * @param  {[type]} options [description]
  * @return {[type]}         [description]
  */
 module.exports.__express = function (options) {
-	var app = options.app;
-
-	return function (req, res, next) {
-		var root = new Bifocals(res);
-		root.dir = app.get('views') + '/';
-		root.content_type = 'text/html';
-
-		res.child = function (key, template) {
-			return root.child(key, template);
-		};
-
-		res.render = function (name, opts, fn) {
-			if (typeof opts === "object") {
-				Object.keys(opts).forEach(function (value, key) {
-					root.set(key, value);
-				});
-			}
-
-			if (typeof fn === "function") {
-				root.error(function (err) {
-					fn(err);
-				});
-
-				root.end(function() {
-					fn(null);
-				});
-			}
-
-			root.render(name);
-		};
-
-		next();
-	}
-}
+	// lazy load, to help modularize some parts the library.
+	var middleware = require('./lib/express_extensions').middleware;
+	module.exports.__express = middleware;
+	return middleware(options);
+};
